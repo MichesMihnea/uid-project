d42fa2b43cc6c7398f031c0632deb8684587bb4f
Moldovandreii <andrei_moldovan07@yahoo.com>
2020-12-08 23:08:41 +0200

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch, BrowserRouter as Router, Redirect } from 'react-router-dom';
-import LoginContainer from './Login/LoginContainer';
-import RegistrationContainer from './Registration/RegistrationContainer';
-import HomeContainer from './Home/HomeContainer';
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
     <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="/login" />
-            </Route>
-            <Route path="/login">
-              <LoginContainer />
-            </Route>
-            <Route path="/registration">
-              <RegistrationContainer />
-            </Route>
-            <Route path="/home">
-              <HomeContainer />
-            </Route>
-          </Switch>
-        </Router>
+        <Navbar />
       </div>
   );
 }
