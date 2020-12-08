52528af697925fe5f01c1b2999840c0911a10bca
Moldovandreii <andrei_moldovan07@yahoo.com>
2020-12-08 14:36:11 +0200

    Setup
    Setup initialisation

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react"
+
+class HomePage extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>This is the home page</h1>
+            </div>
+        )
+    }
+}
+
+export default HomePage
