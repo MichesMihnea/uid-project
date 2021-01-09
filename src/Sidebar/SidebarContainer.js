84c6edbe8e014ee653b1b04198bdeb51ed7d2b37
MichesMihnea <mihnea_m2879@yahoo.com>
2021-01-09 16:38:55 +0200

    Restricted access to request creation page to beneficiary only
    Signed-off-by: MichesMihnea <mihnea_m2879@yahoo.com>

--- src/Sidebar/SidebarContainer.js
+++ src/Sidebar/SidebarContainer.js
@@ -3,7 +3,35 @@ import Sidebar from './Sidebar'
 
 function SidebarContainer() {
 
-    const items = [
+    var items = [];
+    if(localStorage.getItem("Type") === "beneficiary")
+      items = [
+          { name: 'home', label: 'Home' },
+          {
+            name: 'myProfile',
+            label: 'My Profile',
+            items: [
+              { name: 'seeProfile', label: 'See Profile' },
+              { name: 'editProfile', label: 'Edit Profile' },
+              { name: 'addRequest', label: 'Add a request' },
+              { name: 'logOut', label: 'LOG OUT' },
+            ],
+          },
+          {
+            name: 'announcements',
+            label: 'Announcements',
+          },
+          {
+            name: 'settings',
+            label: 'Settings',
+          },
+          {
+            name: 'faq',
+            label: 'FAQ',
+          }
+        ]
+    else 
+      items = [
         { name: 'home', label: 'Home' },
         {
           name: 'myProfile',
@@ -11,7 +39,6 @@ function SidebarContainer() {
           items: [
             { name: 'seeProfile', label: 'See Profile' },
             { name: 'editProfile', label: 'Edit Profile' },
-            { name: 'addRequest', label: 'Add a request' },
             { name: 'logOut', label: 'LOG OUT' },
           ],
         },
