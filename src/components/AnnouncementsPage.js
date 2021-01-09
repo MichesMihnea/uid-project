4521204f4b7e3175e0ccb5765f605d6284b6568b
MichesMihnea <mihnea_m2879@yahoo.com>
2021-01-09 14:47:36 +0200

    Added announcements and faq pages

--- src/components/AnnouncementsPage.js
+++ src/components/AnnouncementsPage.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react'
+import '../App.css';
+import Button from '@material-ui/core/Button';
 
-class AnnouncementsPage extends Component{
+function AnnouncementsPage() {
 
-    render(){
-        return(
-            <div>
-                <h1>Announcements Page</h1>
+    const announcements = [
+        {name: "John", type: "electrical"},
+        {name: "Marius", type: "plumbing"}
+    ]
+
+    return(
+        <div>
+            <h1>Your Announcements</h1>
+            <div className="add-request">
+                {announcements.map((announcement) => (
+                    <div className="announcement-wrapper">
+                        <h4 style={{"marginBottom": "20px"}}>{announcement.name} wants to respond to your {announcement.type} request.</h4>
+                        <span style={{"marginRight": "10%"}}>
+                            <Button variant="contained" color="primary">
+                                Approve
+                            </Button>
+                        </span>
+                        <span>
+                            <Button variant="contained" color="secondary">
+                                See profile
+                            </Button>
+                        </span>
+                    </div>
+                ))            
+            }
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default AnnouncementsPage
--- src/components/FaqPage.js
+++ src/components/FaqPage.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react'
 
-class FaqPage extends Component{
+function FaqPage() {
 
-    render(){
-        return(
-            <div>
-                <h1>FAQ Page</h1>
+    const questions = [
+        {question: "Question 1", answer: "Answer 1"},
+        {question: "Question 2", answer: "Answer 2"}
+    ]
+
+    return(
+        <div>
+            <h1>Your Announcements</h1>
+            <div className="add-request">
+                {questions.map((question) => (
+                    <div className="announcement-wrapper">
+                        <h3 style={{"marginBottom": "20px"}}>Q: {question.question}</h3>
+                        <h4 style={{"marginBottom": "20px"}}>A: {question.answer}</h4>
+                    </div>
+                ))            
+            }
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default FaqPage
